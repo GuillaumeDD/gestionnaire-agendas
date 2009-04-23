39fa2459d22118cec8c76b49153ad5632a4c42d5
pauline.requena <pauline.requena@91d3ccca-f91b-11dd-a8e2-f7cb3c35fcff>
2009-04-23 17:06:56 +0000

    Codage partie GestionAgenda Problème : comment on définit l'ID lors d'une création d'agenda ou d'evenement ?

--- trunk/GestionnaireAgendas/src/java/GestionAgenda/PortefeuilleAgenda.java
+++ trunk/GestionnaireAgendas/src/java/GestionAgenda/PortefeuilleAgenda.java
@@ -2,54 +2,93 @@ package GestionAgenda;
 
 import Authentification.Utilisateur; 
 import java.sql.Date;
-import java.util.ArrayList; 
+import java.util.HashMap;
+import service.PortefeuilleAgendaDAO;
 
 public class PortefeuilleAgenda {
 
     private Utilisateur utilisateur;
 
-    private ArrayList<Agenda> agendas;
+    private HashMap<Integer,Agenda> agendas;
 
     public PortefeuilleAgenda () {
     }
 
     public void creerAgenda (String name, String description, String lieu, String color) {
+        Agenda a = new Agenda(name,description,lieu,color,utilisateur.getUserID());
+        a.verifierValiditeNom(name);
+        if(estUniqueNomAgenda(name))
+            ajouterAgenda(a);
     }
 
     public boolean estUniqueNomAgenda (String name) {
-        return true;
+       boolean result = true;
+       for(Agenda boucle:agendas.values())
+            if(boucle.getNom().equals(name)) result = false;
+       return result;
     }
 
     public void ajouterAgenda (Agenda a) {
+        agendas.put(a.getAgendaID(), a);
     }
 
     public void creerEvenement (int agendaID, String objet, String lieu, String description, Date d, int heureDebut, int heureFin) {
+    Agenda a = new Agenda();
+    a = getAgenda(agendaID);
+    Evenement evt = new Evenement(agendaID,objet,lieu,description,d,heureDebut,heureFin);
+    if(evt.estValideEvenement()==true)
+        a.ajouterEvenement(evt);
     }
 
     public Agenda getAgenda (int agendaID) {
-        return null;
+        return agendas.get(agendaID);
     }
 
     public void modifierAgenda (int agendaID, String name, String description, String lieu, String color) {
+        Agenda ag = new Agenda();
+        ag = getAgenda(agendaID);
+        if(ag.verifierValiditeNom(name)==true)
+            if(estUniqueNomAgenda(name))
+                {
+                ag.setNom(name);
+                ag.setDescription(description);
+                ag.setLieu(lieu);
+                ag.setColor(color);
+                agendas.remove(agendaID);
+                agendas.put(agendaID, ag);
+                }
     }
 
     public void modifierEvenement (int agendaID, int eventID, String objet, String lieu, String description, Date d, int heureDebut, int heureFin) {
+    Agenda ag = new Agenda();
+    ag = getAgenda(agendaID);
+    ag.modifierEvenement(eventID, objet, lieu, description, d, heureDebut, heureFin);
+    agendas.remove(agendaID);
+    agendas.put(agendaID, ag);
     }
 
     public void supprimerAgenda (int agendaID) {
+    Agenda ag = new Agenda();
+    ag = getAgenda(agendaID);
+    ag.supprimer();
+    agendas.remove(agendaID);
     }
 
     public void supprimerEvenement (int agendaID, int eventID) {
+    Agenda ag = new Agenda();
+    ag = getAgenda(agendaID);
+    ag.supprimerEvenement(eventID);
     }
 
     public void initialiser () {
+        //PortefeuilleAgendaDAO a = new PortefeuilleAgendaDAO();
     }
 
-    public ArrayList<Agenda> getAgendas () {
+    public HashMap<Integer,Agenda> getAgendas () {
         return agendas;
     }
 
-    public void setAgendas (ArrayList<Agenda> val) {
+    public void setAgendas (HashMap<Integer,Agenda> val) {
         this.agendas = val;
     }
 
